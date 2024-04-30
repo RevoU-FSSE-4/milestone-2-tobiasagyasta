9a3d9fd8987c25cecfbc4de7a8321d6ecce877b4
Tobias Halomoan <agyasta1808@gmail.com>
2024-05-01 01:46:36 +0700

    Added weather-icons icon package.

--- src/pages/Display.tsx
+++ src/pages/Display.tsx
@@ -1,6 +1,7 @@
 import data from "../data/DummyData.json";
 import TimeFetcher from "../components/TimeFetcher";
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+import "../styles/css/weather-icons.css";
 
 const Display = () => {
 	polyfillCountryFlagEmojis();
@@ -23,22 +24,11 @@ const Display = () => {
 					<div className='text-sm text-gray-500'>
 						<TimeFetcher></TimeFetcher>
 					</div>
-					<div className='mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24'>
-						<svg
-							className='w-32 h-32'
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
-								d='M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z'
-							></path>
-						</svg>
-					</div>
+
+					<i
+						className={`my-3 inline-flex items-center justify-center wi wi-owm-${data.weather[0].id} text-blue-400 text-8xl`}
+					></i>
+
 					<div className='flex flex-row items-center justify-center mt-6'>
 						<div className=' flex flex-col text-center items-center'>
 							<span className=' relative font-medium text-6xl left-2'>
